22a2e57a91b730d66ba37ce6e16a13f24d3a61a1
dimitar.b <dimitar.b@cooltecho.com>
2021-10-19 17:56:21 +0300

    add address for the constructor

--- contracts/base/InitialCoinOfferingContractBase.sol
+++ contracts/base/InitialCoinOfferingContractBase.sol
@@ -9,26 +9,38 @@ abstract contract InitialCoinOfferingConractBase is IInicialCoinOffering {
     uint256 private startDateTimestamp; //startDate of the ICO in second
     uint256 private endDateTimestamp; //end Date of the ICO in second
     uint256 private blockTimeStamp; //The block time stamp at the moment of deploy.
-    
-    constructor(uint256 _startDateTimestamp, uint256 _endDateTimeStamp) {
+    address private contractAddress;
+
+    constructor(uint256 _startDateTimestamp, uint256 _endDateTimeStamp, address _contractAddress) {
         //On Deploy the ICO is NOT Started
         status = InitialCoinOfferingStatus.NotStarted; 
         _owner = msg.sender; //setting the owner of the ICO 
-        blockTimeStamp = block.timestamp;
+        blockTimeStamp = block.timestamp; //set the last block timestamp
+
+        //Perform basic validation for the start and end date of the ICO
         require(blockTimeStamp <= startDateTimestamp, "The ICO cannot start in the past");
         require(_startDateTimestamp < _endDateTimeStamp, "Start date must be before End Date");
 
         startDateTimestamp = _startDateTimestamp;
         endDateTimestamp = _endDateTimeStamp;
-
-        
+        //TODO: check if the address is ERC20 
+        contractAddress = _contractAddress;
     }
 
-    function buy() public virtual override payable returns(bool) {
+    modifier started {
+        require(status == InitialCoinOfferingStatus.InProgress, "The ICO is not started yet!");
+        _;
+    }
+
+    modifier notStarted {
+        require(status == InitialCoinOfferingStatus.NotStarted , "The ICO is either in progress or finished");
+        _;
+    }
+    function buy() public virtual override payable started returns(bool) {
         return false;
     }
 
-    function start() public virtual override returns(InitialCoinOfferingStatus) {
+    function start() public virtual override notStarted returns(InitialCoinOfferingStatus) {
         return status;
     }
 
